bcc564ca168457d1a074d1ef173829e79eb15723
Andrei Rauh <rauhandrei69@gmail.com>
2021-04-29 12:50:52 +0300

    Create UI

---
+++ ByteCoinPrices/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  ByteCoinPrices
+//
+//  Created by Rauh Andrei on 29.04.2021.
+//  Copyright © 2021 Rauh Andrei. All rights reserved.
+//
+
+import Foundation
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
